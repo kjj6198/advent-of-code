b701ac80f181b574e9e3c4b4428e63371f1fbc3a
Kalan <kjj6198@gmail.com>
2024-12-06 11:54:26 +0000

    Add day6

---
+++ js/2024/day6/main.js
@@ -0,0 +1,13 @@
+
+export function part1(input) {
+    input = input.split('\n')
+    const rows = input.length;
+    const cols = input[0].length;
+
+    const directions = [
+        [-1, 0],
+        [0, 1],
+        [1, 0],
+        [0, -1],
+    ];
+}
